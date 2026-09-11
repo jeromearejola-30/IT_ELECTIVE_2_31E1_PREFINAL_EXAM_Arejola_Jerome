0a1f0caa64accd737ce4d98487c2d423106c2453
jeromearejola-30 <jeromearejola30@gmail.com>
2026-09-11 14:12:46 +0800

    12

--- IT_ELECTIVE_2_31E1_PREFINAL_EXAM_Arejola_Jerome/Controllers/HomeController.cs
+++ IT_ELECTIVE_2_31E1_PREFINAL_EXAM_Arejola_Jerome/Controllers/HomeController.cs
@@ -116,6 +116,15 @@ namespace PrefinalExamApp.Controllers
                     Topic = "ViewModels & Presentation",
                     Explanation = "ViewModels decouple UI/view requirements from entity models."
                 },
+                new ExamQuestion
+                {
+                    Id = 12,
+                    QuestionText = "Consider this query: var students = _context.Students.Include(s => s.Section).ToList(); What is the main benefit of Include(s => s.Section)?",
+                    Options = new List<string>{ "A. It loads the related Section navigation property", "B. It creates a Section object manually", "C. It removes the foreign key", "D. It prevents the query from accessing the database" },
+                    CorrectAnswer = "A",
+                    Topic = "LINQ Query Optimization",
+                    Explanation = "It populates the Section navigation property directly inside the student query."
+                },
             };
         }
     }
